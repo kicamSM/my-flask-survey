2c222211a244e21c9a17c99d2dafc95c994c50c9
kicamSM <kicamSMM@hotmail.com>
2023-03-24 13:01:59 -0600

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -10,7 +10,8 @@ app.config['SECRET_KEY'] = "iloverollerderby12"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False 
 debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_ANSWERS = 'responses'
+# responses = []
 
 @app.route("/")
 def home_page():
@@ -20,37 +21,50 @@ def home_page():
     
     return render_template('home.html', instructions=instructions, title=title)
 
-@app.route('/question/<int:i>')
-def displays_questions(i):
-    """Displays the questions in survey"""
-    if len(responses) is None:  
-        return redirect("/")
-    if len(responses) != i:
-        flash("Please finish the survey.", 'error')
-        return redirect(f"/question/{len(responses)}")
-    if len(responses) is i + 1: 
-        return redirect('/thank_you')
-    if i >= len(satisfaction_survey.questions):
-        flash("Please finish the survey.", 'error')
-        return redirect(f"/question/{len(responses)}")
-    if i < len(satisfaction_survey.questions):
-        question = satisfaction_survey.questions[i].question
-        return render_template('questions.html', question=question)
-
-    
-    # else: question = "Post not found."
-    # return render_template('questions.html', question=question)
+@app.route('/verify_user', methods=["POST"])
+def verify_user():
+    session[RESPONSES_ANSWERS] = []
+    return redirect('/question/0')
 
 @app.route('/answer', methods=["POST"])
 def sends_answers(): 
     """Logging answers to survey"""
+    # session["answer"] = request.form["answer"]
+    # answer = session["answer"]
+    responses = session[RESPONSES_ANSWERS]
+    
     answer = request.form['answer']
     responses.append(answer)
+    session[RESPONSES_ANSWERS] = responses
+    # responses.append(answer)
     i = len(responses)
     if i < len(satisfaction_survey.questions): 
         return redirect(f"/question/{i}")
     else: 
         return redirect('/thank_you')
+
+@app.route('/question/<int:i>')
+def displays_questions(i):
+    """Displays the questions in survey"""
+    responses = session.get(RESPONSES_ANSWERS)
+    # i think we are obtaining the responses_answers here 
+    
+    if (responses is None):  
+    # if len(responses) is None:  
+        return redirect("/")
+    
+    # if len(responses) != i:
+    if len(responses) != i:
+        flash(f"Please answer question {len(responses) + 1} of the survey.", 'error')
+        return redirect(f"/question/{len(responses)}")
+    
+    if i == len(satisfaction_survey.questions):
+        return redirect('thank_you')
+
+    if i < len(satisfaction_survey.questions):
+        question = satisfaction_survey.questions[i].question
+        return render_template('questions.html', question=question)
+
 
 @app.route('/thank_you')
 def thank_you():
